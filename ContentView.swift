763cc4f0fd60ed20e96a4473853795f074b2b002
Arthur <109434166+Arturhackfox@users.noreply.github.com>
2022-07-30 15:49:05 +0300

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,110 @@
+//
+//  ContentView.swift
+//  War Challenge 90 days with Chris
+//
+//  Created by Arthur Sh on 27.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card7"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    
+    var body: some View {
+        ZStack{
+            
+            Image("background")
+                .ignoresSafeArea()
+            
+            
+            VStack{
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button {
+                    
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    
+                } label: {
+                    Image("dealbutton")
+                }
+                
+                
+                
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    
+                    VStack{
+                        
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 5)
+                        
+                        Text(String(playerScore))
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                    VStack{
+                        
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 5)
+                        
+                        
+                        Text(String(cpuScore))
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                    
+                }
+                
+                
+                Spacer()
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
